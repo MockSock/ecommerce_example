275c5859d32f8ed2e33883692100d57e8e4be786
madden20202 <business.mocksock@gmail.com>
2022-10-27 15:47:26 -0500

    add basic product.dart

---
+++ lib/model/product.dart
@@ -0,0 +1,11 @@
+class Product {
+  Product({
+    required this.price,
+    required this.title,
+    required this.description,
+  });
+
+  final int price;
+  final String title;
+  final String description;
+}
