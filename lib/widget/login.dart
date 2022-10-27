b534efb01b490f5565f55603234fe4b17d9e8312
madden20202 <business.mocksock@gmail.com>
2022-10-27 15:58:55 -0500

    add skeleton work for LoginPage widget

---
+++ lib/widget/login.dart
@@ -0,0 +1,15 @@
+import 'dart:html';
+
+import 'package:flutter/cupertino.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Text('I work!');
+  }
+}
