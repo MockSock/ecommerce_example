cb9827f8b30941ba87b45189d4c19ca025215e02
madden20202 <business.mocksock@gmail.com>
2022-10-28 17:16:35 -0500

    add a route name

--- lib/widget/login.dart
+++ lib/widget/login.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
+  static const routeName = '/auth';
+
   @override
   Widget build(BuildContext context) {
     return Container(
