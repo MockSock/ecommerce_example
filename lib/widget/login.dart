6beb4ad3d67f30ce6a83ce0096092fce720d4b35
madden20202 <business.mocksock@gmail.com>
2022-10-28 17:29:59 -0500

    Add a container that is a gradient

--- lib/widget/login.dart
+++ lib/widget/login.dart
@@ -7,11 +7,28 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final deviceSize = MediaQuery.of(context).size;
     return Container(
       alignment: Alignment.center,
       padding: const EdgeInsets.all(12.0),
       margin: const EdgeInsets.all(8.0),
-      child: const Text('Test 123'),
+      child: Stack(
+        children: <Widget>[
+          Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Colors.cyan,
+                  Colors.white,
+                ],
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                stops: [0, 1],
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
