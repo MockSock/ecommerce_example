57c4d4fc4221f2f592dce366ff072d80ea7e1579
madden20202 <business.mocksock@gmail.com>
2022-10-29 06:05:25 -0500

    modify products to have ids

--- lib/model/product.dart
+++ lib/model/product.dart
@@ -1,10 +1,12 @@
 class Product {
   Product({
+    required this.id,
     required this.price,
     required this.title,
     required this.description,
   });
 
+  final String id;
   final int price;
   final String title;
   final String description;
